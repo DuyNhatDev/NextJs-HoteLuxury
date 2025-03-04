2138e94320ec4115f34a9522db6748c33e24818e
DuyNhatDev <duynhat1507@gmail.com>
2025-03-05 00:18:58 +0700

    Tạo refresh token route handler

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -3,6 +3,8 @@ import {
   LoginBodyType,
   LoginResType,
   LogoutBodyType,
+  RefreshTokenBodyType,
+  RefreshTokenResType,
   RegisterBodyType,
   RegisterResType,
   VerifyAccountBodyType,
@@ -35,5 +37,11 @@ const authApiRequest = {
       }
     ),
   logout: () => http.post('/api/auth/logout', null, { baseUrl: '' }),
+  sRefreshToken: (body: RefreshTokenBodyType) =>
+    http.post<RefreshTokenResType>('/auth/refresh-token', body),
+  refreshToken: () =>
+    http.post<RefreshTokenResType>('/api/auth/refresh-token', null, {
+      baseUrl: '',
+    }),
 }
 export default authApiRequest
