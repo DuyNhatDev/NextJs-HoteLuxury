c5b2c9091ca5bc2d6a884290f38ff68707d67509
DuyNhat157 <duynhat1507@gmail.com>
2025-04-10 07:49:19 +0700

    Giao diện search form

--- src/app/(public)/components/search-form.tsx
+++ src/app/(public)/components/search-form.tsx
@@ -14,15 +14,17 @@ import {
   CommandShortcut,
 } from '@/components/ui/command'
 import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
-import { Popover } from '@/components/ui/popover'
 import { SearchSuggestSchema, SearchSuggestType } from '@/schemaValidations/search.schema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { addDays } from 'date-fns'
-import { useRef, useState } from 'react'
+import { Calculator, Calendar, CreditCard, MapPin, Settings, Smile, User } from 'lucide-react'
+import { useEffect, useRef, useState } from 'react'
 import { DateRange } from 'react-day-picker'
 import { useForm } from 'react-hook-form'
 
 export default function SearchForm() {
+  const [open, setOpen] = useState(false)
+  const wrapperRef = useRef<HTMLDivElement>(null)
   const form = useForm<SearchSuggestType>({
     resolver: zodResolver(SearchSuggestSchema),
     defaultValues: {
@@ -31,6 +33,17 @@ export default function SearchForm() {
       keyword: '',
     },
   })
+  const handleClickOutside = (event: MouseEvent) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target as Node)) {
+      setOpen(false)
+    }
+  }
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
   const onSubmit = async (data: SearchSuggestType) => {
     console.log('From', data.dayStart)
     console.log('To', data.dayEnd)
@@ -46,7 +59,57 @@ export default function SearchForm() {
               console.log(err)
             })}
           >
-            {/* <div className="relative w-full"></div> */}
+            <div className="relative w-full">
+              <div ref={wrapperRef} className="relative w-full">
+                <Command className="w-full rounded-lg border shadow-md">
+                  <CommandInput
+                    placeholder="Bạn muốn đi đâu"
+                    icon={<MapPin className="size-4 shrink-0 opacity-50" />}
+                    className="h-14 rounded-lg"
+                    onFocus={() => setOpen(true)}
+                  />
+                  {open && (
+                    <div className="absolute top-full z-20 mt-1 w-full rounded-md border bg-white shadow-md">
+                      <CommandList>
+                        <CommandEmpty>No results found.</CommandEmpty>
+                        <CommandGroup heading="Suggestions">
+                          <CommandItem>
+                            <Calendar />
+                            <span>Calendar</span>
+                          </CommandItem>
+                          <CommandItem>
+                            <Smile />
+                            <span>Search Emoji</span>
+                          </CommandItem>
+                          <CommandItem disabled>
+                            <Calculator />
+                            <span>Calculator</span>
+                          </CommandItem>
+                        </CommandGroup>
+                        <CommandSeparator />
+                        <CommandGroup heading="Settings">
+                          <CommandItem>
+                            <User />
+                            <span>Profile</span>
+                            <CommandShortcut>⌘P</CommandShortcut>
+                          </CommandItem>
+                          <CommandItem>
+                            <CreditCard />
+                            <span>Billing</span>
+                            <CommandShortcut>⌘B</CommandShortcut>
+                          </CommandItem>
+                          <CommandItem>
+                            <Settings />
+                            <span>Settings</span>
+                            <CommandShortcut>⌘S</CommandShortcut>
+                          </CommandItem>
+                        </CommandGroup>
+                      </CommandList>
+                    </div>
+                  )}
+                </Command>
+              </div>
+            </div>
             <div className="grid grid-cols-8 items-stretch gap-4">
               <div className="col-span-4">
                 <FormField
@@ -76,7 +139,10 @@ export default function SearchForm() {
               </div>
 
               <div className="col-span-1 h-full">
-                <Button type="submit" className="h-full w-full bg-orange-500 hover:bg-orange-600">
+                <Button
+                  type="submit"
+                  className="h-full w-full bg-orange-400 font-bold hover:bg-orange-500"
+                >
                   Tìm
                 </Button>
               </div>
--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -49,14 +49,18 @@ function CommandDialog({
     </Dialog>
   )
 }
+interface CommandInputProps extends React.ComponentProps<typeof CommandPrimitive.Input> {
+  icon?: React.ReactNode
+}
 
 function CommandInput({
+  icon = <SearchIcon className="size-4 shrink-0 opacity-50" />,
   className,
   ...props
-}: React.ComponentProps<typeof CommandPrimitive.Input>) {
+}: CommandInputProps) {
   return (
-    <div data-slot="command-input-wrapper" className="flex h-9 items-center gap-2 border-b px-3">
-      <SearchIcon className="size-4 shrink-0 opacity-50" />
+    <div data-slot="command-input-wrapper" className="flex items-center gap-2 border-b px-3">
+      {icon}
       <CommandPrimitive.Input
         data-slot="command-input"
         className={cn(
