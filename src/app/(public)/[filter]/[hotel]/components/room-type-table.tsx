7a1830624eb0eca49a2cf33e8387bb92f2229bb9
DuyNhat157 <duynhat1507@gmail.com>
2025-04-25 23:00:27 +0700

    Chỉnh sửa button yêu cầu đặt phòng

--- src/app/(public)/[filter]/[hotel]/components/room-type-table.tsx
+++ src/app/(public)/[filter]/[hotel]/components/room-type-table.tsx
@@ -44,114 +44,130 @@ export default function RoomTypeTable({ params }: RoomTypeTableProps) {
     setMinPrice(price)
   }, [price, setMinPrice])
 
-return (
-  <>
-    {roomTypeFilterListQuery.isPending ? (
-      <div className="flex items-center justify-center py-8">
-        <Spinner>Đang tìm kiếm phòng. Xin vui lòng chờ trong giây lát</Spinner>
-      </div>
-    ) : roomTypeList.length === 0 ? (
-      <p className="px-4 py-8 text-center text-lg">Không tìm thấy kết phù hợp</p>
-    ) : (
-      <Table className="mt-4 w-full table-fixed border-collapse border">
-        <TableHeader className="bg-gray-200">
-          <TableRow>
-            <TableHead className="w-[26.94%] border text-lg font-semibold text-blue-900">Loại phòng</TableHead>
-            <TableHead className="w-[31.09%] border text-center text-lg font-semibold text-blue-900">Mô tả</TableHead>
-            <TableHead className="w-[23.32%] border text-center text-lg font-semibold text-blue-900">Tổng giá</TableHead>
-            <TableHead className="w-[18.65%] border text-right text-lg font-semibold text-blue-900">Giữ phòng</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {roomTypeList.map((rt) => {
-            const listImage = [...rt.roomTypeImages, ...rt.roomTypeImages]
-            return (
-              <TableRow key={rt.roomTypeId} className="hover:bg-transparent">
-                {/* Loại phòng */}
-                <TableCell className="w-[26.94%] border py-3 align-top font-medium">
-                  <h1 className="mb-2 text-base font-semibold whitespace-normal text-sky-500">
-                    {rt.roomTypeName}
-                  </h1>
-                  <Dialog>
-                    <DialogTrigger asChild>
-                      <div>
-                        <div className="relative col-span-3 aspect-[3/2] h-full w-full overflow-hidden hover:cursor-pointer">
-                          <Image
-                            src={rt.roomTypeImage}
-                            alt={rt.roomTypeName}
-                            fill
-                            className="object-cover"
-                          />
-                        </div>
-                        <p className="mt-2 text-sm font-normal text-sky-500 hover:cursor-pointer">
-                          Xem ảnh chi tiết phòng
-                        </p>
-                      </div>
-                    </DialogTrigger>
-                    <DialogContent className="flex w-[720px] !max-w-3xl flex-col border-none bg-transparent p-4">
-                      <DialogHeader className="flex-shrink-0">
-                        <DialogTitle className="text-blue-500">{rt.roomTypeName}</DialogTitle>
-                      </DialogHeader>
-                      <Carousel>
-                        <CarouselPrevious className="absolute top-1/2 left-4 z-10 h-7 w-7 -translate-y-1/2 opacity-50" />
-                        <CarouselNext className="absolute top-1/2 right-4 z-10 h-7 w-7 -translate-y-1/2 opacity-50" />
-                        <div className="relative">
-                          <CarouselMainContainer className="h-[443px] w-[686px]">
-                            {listImage.map((img, index) => (
-                              <SliderMainItem key={index} className="h-full w-full bg-transparent">
-                                <div className="relative h-full w-full overflow-hidden">
-                                  <Image
-                                    src={img}
-                                    alt={`hotel-image-${index}`}
-                                    quality={100}
-                                    fill
-                                    className="object-cover object-center"
-                                  />
-                                </div>
-                              </SliderMainItem>
-                            ))}
-                          </CarouselMainContainer>
-                          <div className="absolute bottom-2 left-1/2 -translate-x-1/2">
-                            <CarouselThumbsContainer className="gap-x-1">
-                              {listImage.map((_, index) => (
-                                <CarouselIndicator key={index} index={index} />
-                              ))}
-                            </CarouselThumbsContainer>
+  return (
+    <>
+      {roomTypeFilterListQuery.isPending ? (
+        <div className="flex items-center justify-center py-8">
+          <Spinner>Đang tìm kiếm phòng. Xin vui lòng chờ trong giây lát</Spinner>
+        </div>
+      ) : roomTypeList.length === 0 ? (
+        <p className="px-4 py-8 text-center text-lg">Không tìm thấy kết phù hợp</p>
+      ) : (
+        <Table className="mt-4 w-full table-fixed border-collapse border">
+          <TableHeader className="bg-gray-200">
+            <TableRow>
+              <TableHead className="w-[26.94%] border text-lg font-semibold text-blue-900">
+                Loại phòng
+              </TableHead>
+              <TableHead className="w-[31.09%] border text-center text-lg font-semibold text-blue-900">
+                Mô tả
+              </TableHead>
+              <TableHead className="w-[23.32%] border text-center text-lg font-semibold text-blue-900">
+                Tổng giá
+              </TableHead>
+              <TableHead className="w-[18.65%] border text-right text-lg font-semibold text-blue-900">
+                Giữ phòng
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {roomTypeList.map((rt) => {
+              const listImage = [...rt.roomTypeImages, ...rt.roomTypeImages]
+              return (
+                <TableRow key={rt.roomTypeId} className="hover:bg-transparent">
+                  {/* Loại phòng */}
+                  <TableCell className="w-[26.94%] border py-3 align-top font-medium">
+                    <h1 className="mb-2 text-base font-semibold whitespace-normal text-sky-500">
+                      {rt.roomTypeName}
+                    </h1>
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <div>
+                          <div className="relative col-span-3 aspect-[3/2] h-full w-full overflow-hidden hover:cursor-pointer">
+                            <Image
+                              src={rt.roomTypeImage}
+                              alt={rt.roomTypeName}
+                              fill
+                              className="object-cover"
+                            />
                           </div>
+                          <p className="mt-2 text-sm font-normal text-sky-500 hover:cursor-pointer">
+                            Xem ảnh chi tiết phòng
+                          </p>
                         </div>
-                      </Carousel>
-                    </DialogContent>
-                  </Dialog>
-                </TableCell>
+                      </DialogTrigger>
+                      <DialogContent className="flex w-[720px] !max-w-3xl flex-col border-none bg-transparent p-4">
+                        <DialogHeader className="flex-shrink-0">
+                          <DialogTitle className="text-blue-500">{rt.roomTypeName}</DialogTitle>
+                        </DialogHeader>
+                        <Carousel>
+                          <CarouselPrevious className="absolute top-1/2 left-4 z-10 h-7 w-7 -translate-y-1/2 opacity-50" />
+                          <CarouselNext className="absolute top-1/2 right-4 z-10 h-7 w-7 -translate-y-1/2 opacity-50" />
+                          <div className="relative">
+                            <CarouselMainContainer className="h-[443px] w-[686px]">
+                              {listImage.map((img, index) => (
+                                <SliderMainItem
+                                  key={index}
+                                  className="h-full w-full bg-transparent"
+                                >
+                                  <div className="relative h-full w-full overflow-hidden">
+                                    <Image
+                                      src={img}
+                                      alt={`hotel-image-${index}`}
+                                      quality={100}
+                                      fill
+                                      className="object-cover object-center"
+                                    />
+                                  </div>
+                                </SliderMainItem>
+                              ))}
+                            </CarouselMainContainer>
+                            <div className="absolute bottom-2 left-1/2 -translate-x-1/2">
+                              <CarouselThumbsContainer className="gap-x-1">
+                                {listImage.map((_, index) => (
+                                  <CarouselIndicator key={index} index={index} />
+                                ))}
+                              </CarouselThumbsContainer>
+                            </div>
+                          </div>
+                        </Carousel>
+                      </DialogContent>
+                    </Dialog>
+                  </TableCell>
 
-                {/* Mô tả */}
-                <TableCell className="w-[31.09%] border align-top">
-                  <div
-                    className="mt-2 leading-relaxed whitespace-normal"
-                    dangerouslySetInnerHTML={{ __html: rt.roomTypeDescription || '' }}
-                  ></div>
-                </TableCell>
+                  {/* Mô tả */}
+                  <TableCell className="w-[31.09%] border align-top">
+                    <div
+                      className="mt-2 leading-relaxed whitespace-normal"
+                      dangerouslySetInnerHTML={{ __html: rt.roomTypeDescription || '' }}
+                    ></div>
+                  </TableCell>
 
-                {/* Tổng giá */}
-                <TableCell className="w-[23.32%] border text-center align-top">
-                  <p className="mt-2 text-lg font-semibold text-sky-500">
-                    {Number(rt.roomTypePrice).toLocaleString('vi-VN')}{' '}
-                    <span className="text-sm">VND</span>
-                  </p>
-                </TableCell>
+                  {/* Tổng giá */}
+                  <TableCell className="w-[23.32%] border text-center align-top">
+                    <p className="mt-2 text-lg font-semibold text-sky-500">
+                      {Number(rt.roomTypePrice).toLocaleString('vi-VN')}{' '}
+                      <span className="text-sm">VND</span>
+                    </p>
+                  </TableCell>
 
-                {/* Đặt */}
-                <TableCell className="mt-2 w-[18.65%] border text-right align-top">
-                  <Button className="mt-2 rounded bg-orange-400 text-[15px] font-bold hover:bg-orange-400">
-                    Yêu cầu đặt
-                  </Button>
-                </TableCell>
-              </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
-    )}
-  </>
-)
+                  {/* Đặt */}
+                  <TableCell className="mt-2 w-[18.65%] border text-right align-top">
+                    <div className="mt-2 inline-block text-center">
+                      <Button className="rounded-t rounded-b-none bg-orange-400 px-6 py-2 text-[15px] font-bold text-white hover:bg-orange-400">
+                        Yêu cầu đặt
+                      </Button>
+                      <div className="w-full rounded-b border border-orange-400 bg-white px-6 py-1 text-[14px] text-orange-400">
+                        {params.currentRooms} phòng
+                      </div>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      )}
+    </>
+  )
 }
