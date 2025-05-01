1acc41a5f15255b69b2e4a70becb7d006b90bd88
DuyNhat157 <duynhat1507@gmail.com>
2025-05-01 14:49:51 +0700

    Tạo trang 404 not found

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,23 +6,20 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 })
 
 const eslintConfig = [
-  ...compat.extends(
-    'next/core-web-vitals',
-    'next/typescript',
-    'plugin:@tanstack/query/recommended'
-  ),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:@tanstack/query/recommended'),
   {
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
       'no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
-    },
-  },
+      '@next/next/no-img-element': 'off'
+    }
+  }
 ]
 
 export default eslintConfig
