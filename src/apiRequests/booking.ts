5e090da0059fb576c463c86768c39109eac936d2
DuyNhat157 <duynhat1507@gmail.com>
2025-05-02 13:17:51 +0700

    Fix

--- src/apiRequests/booking.ts
+++ src/apiRequests/booking.ts
@@ -1,12 +1,8 @@
 import http from '@/lib/http'
-import { objectToFormData } from '@/lib/utils'
 import { CreateBookingResType, CreateBookingBodyType, BookingListResType } from '@/schemaValidations/booking-schema'
 const prefix = '/booking'
 const bookingApiRequest = {
-  createBooking: (body: CreateBookingBodyType) => {
-    const formData = objectToFormData(body)
-    return http.post<CreateBookingResType>(`${prefix}`, formData)
-  },
+  createBooking: (body: CreateBookingBodyType) => http.post<CreateBookingResType>(`${prefix}`, body),
   getPending: () => http.get<BookingListResType>(`${prefix}?bookingStatus=Chờ xác nhận`),
   getUpcoming: () => http.get<BookingListResType>(`${prefix}?bookingStatus=Đã xác nhận`),
   getProgressing: () => http.get<BookingListResType>(`${prefix}?bookingStatus=Đang thực hiện`),
