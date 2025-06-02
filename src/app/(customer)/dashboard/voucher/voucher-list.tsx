5b51f35aaf4af1b9c90adf47c88ba3eb9a66c78e
DuyNhat157 <duynhat1507@gmail.com>
2025-06-03 06:06:38 +0700

    Hiển thị số lượng voucher

--- src/app/(customer)/dashboard/voucher/voucher-list.tsx
+++ src/app/(customer)/dashboard/voucher/voucher-list.tsx
@@ -19,29 +19,35 @@ export default function VoucherList() {
       </CardHeader>
       <CardContent>
         {listVoucher.map((voucher) => {
+          const { code, discountValue, minOrderValue, quantity } = voucher
           const isFixed = voucher.discountType === 'fixed'
-          const minValue = voucher.minOrderValue
-          const value = voucher.discountValue
           return (
-            <div key={voucher.voucherId} className='my-3 flex items-center gap-2 border p-2'>
-              <div className='relative h-26 w-36 overflow-hidden'>
-                <Image
-                  src={isFixed ? '/image/fixed-voucher.png' : '/image/percentage-voucher.png'}
-                  alt='Hình ảnh'
-                  fill
-                  className='object-cover'
-                />
+            <div
+              key={voucher.voucherId}
+              className='my-3 flex flex-col gap-2 border p-2 md:flex-row md:items-center md:justify-between'
+            >
+              <div className='flex w-full flex-row items-start gap-2 sm:items-center'>
+                <div className='relative h-16 w-20 shrink-0 overflow-hidden sm:h-26 sm:w-36'>
+                  <Image
+                    src={isFixed ? '/image/fixed-voucher.png' : '/image/percentage-voucher.png'}
+                    alt='Hình ảnh'
+                    fill
+                    className='object-cover'
+                  />
+                </div>
+                <div className='flex flex-col items-start text-sm sm:text-base'>
+                  <p className='text-blue-600'>Mã voucher: {code}</p>
+                  <p>
+                    {isFixed ? `Giảm ${discountValue.toLocaleString('vi-VN')}đ` : `Giảm ${discountValue}%`} cho đơn từ{' '}
+                    {minOrderValue.toLocaleString('vi-VN')}đ
+                  </p>
+                  <p className='text-gray-500'>
+                    Hiệu lực đến: {format(parseISO(String(voucher.expiredAt)), 'dd/MM/yyyy')}
+                  </p>
+                </div>
               </div>
-              <div className='flex flex-col items-start'>
-                <p className='text-lg text-blue-600'>Mã voucher: {voucher.code}</p>
-                <p className='text-lg'>
-                  {isFixed ? `Giảm ${value.toLocaleString('vi-VN')}đ` : `Giảm ${value}%`} cho đơn từ{' '}
-                  {minValue.toLocaleString('vi-VN')}đ
-                </p>
-                <p className='text-[15px] text-gray-500'>
-                  Hiệu lực đến: {format(parseISO(String(voucher.expiredAt)), 'dd/MM/yyyy')}
-                </p>
-              </div>
+
+              <p className='text-right text-sm md:mr-3 md:text-lg md:whitespace-nowrap'>x {quantity}</p>
             </div>
           )
         })}
