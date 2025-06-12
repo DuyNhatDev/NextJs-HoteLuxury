14084ea9a41e791fb7d1e1ce0c211330eca2bafa
DuyNhat157 <duynhat1507@gmail.com>
2025-06-12 22:12:22 +0700

    Hiển thị thông tin voucher ở trang chủ

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -7,9 +7,11 @@ import { FeaturedHotelType } from '@/schemas/hotel.schema'
 import hotelApiRequest from '@/api/hotel'
 import FeaturedHotelList from '@/app/(public)/components/featured-hotel-list'
 import voucherApiRequest from '@/api/voucher'
+import { VoucherType } from '@/schemas/voucher.schema'
 export default async function Home() {
   let destinationList: LocationType[] = []
   let featuredHotelList: FeaturedHotelType[] = []
+  let voucher: VoucherType
 
   try {
     const featuredHotelResult = await hotelApiRequest.getFeaturedHotelList()
@@ -27,7 +29,7 @@ export default async function Home() {
 
   try {
     const voucherResult = await voucherApiRequest.getFestivalVoucher()
-    const voucher = voucherResult.payload.data
+    voucher = voucherResult.payload.data
   } catch (error) {
     return <div className='text-center'>Lỗi khi tải danh sách địa điểm</div>
   }
@@ -44,14 +46,25 @@ export default async function Home() {
           priority
         />
 
-        <div className='relative w-full max-w-[800px] px-4'>
-          <div className='absolute -top-22 left-8 space-y-1'>
+        <div className='relative flex w-full max-w-[800px] flex-col items-center space-y-4 px-4'>
+          <div className='absolute -top-22 left-8 hidden space-y-1 md:block'>
             <p className='text-2xl font-bold text-white md:text-4xl'>Đặt phòng dễ dàng</p>
             <p className='text-lg font-semibold text-white md:text-xl'>
               Khách sạn giá tốt nhất, trọn vẹn từng chuyến đi
             </p>
           </div>
+
           <SearchForm />
+
+          {voucher && (
+            <div className='w-full max-w-[400px] space-y-2 rounded-xl bg-white/80 px-4 py-2 shadow-lg backdrop-blur-sm md:absolute md:top-44 md:left-1/2 md:-translate-x-1/2 md:space-y-2 md:px-6 md:py-4'>
+              <p className='text-center text-sm font-bold text-yellow-800 md:text-xl'>🎉 Ưu đãi đặc biệt 🎉</p>
+              <p className='text-center text-sm text-gray-700 md:text-base'>
+                🎁 Nhập mã <span className='font-semibold text-red-600'>{voucher.code}</span>
+              </p>
+              <p className='text-center text-sm text-gray-700 md:text-base'>{voucher.content}</p>
+            </div>
+          )}
         </div>
       </section>
 
